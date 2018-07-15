508f62120d657cd16ddf1b1794eb135c3c3c8a02
Soravis Prakkamakul <asunnotthesun@gmail.com>
2018-07-14 20:58:20 -0700

    Added provider list page

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -2,6 +2,33 @@
 // 1 = after click book -> recommendation page.
 var state = 0;
 
+var templates = {
+	empty: chrome.extension.getURL('template/empty.html'),
+}
+
+var images = {
+	kayakHeader: chrome.extension.getURL('images/kayak-header.png'),
+	kayakFooter: chrome.extension.getURL('images/kayak-footer.png')
+}
+
+var providers = [
+	{
+		name: 'Passport Health Downtown San Francisco Travel Clinic',
+		address: '47 Kearny St STE 701, San Francisco, CA 94108',
+		link: 'https://www.passporthealthusa.com/schedule-appointment/?satid=880'
+	},
+	{
+		name: 'AITC Immunization Travel Clinic',
+		address: '101 Grove St, San Francisco, CA 94102',
+		link: 'https://www.passporthealthusa.com/schedule-appointment/?satid=789'
+	},
+	{
+		name: 'Travel Clinic: California Pacific Medical Center',
+		address: '3801 Sacramento St, San Francisco, CA 94118',
+		link: 'http://www.cpmc.org/services/travel/?utm_medium=local&utm_source=google_plus-local&utm_campaign=local-San-Francisco-CPMC-3'
+	}
+]
+
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -23,6 +50,7 @@ function overrideBookButton() {
 			// if they click this button go to main page...
 			// $('#bookButton').parent().click(function() {launchMainPage(params);});
 			launchMainPage(params)
+			// launchProviderListPage(params)
 		}
 	}, 100);
 }
@@ -36,4 +64,33 @@ function launchMainPage(params) {
 		$('#flight-description').attr('src', chrome.extension.getURL('template/flight-detail.png'));
 		$('#stay-well').attr('src', chrome.extension.getURL('template/stay-well.png'));
 	});
+}
+
+function launchProviderListPage(params) {
+	$.get(templates.empty, function(data) {
+		$('.col-main').empty();
+	  $('.col-rr').empty();
+	  $('.Checkout-Common-Title-BannerTitle h1').html("Trip > Booking receipts > KUSW-292905");
+		$($.parseHTML(data)).appendTo('.col-main');
+
+		$('.provider-list').append(createProviderListItem(providers[0]))
+		$('.provider-list').append(createProviderListItem(providers[1]))
+		$('.provider-list').append(createProviderListItem(providers[2]))
+	});
+}
+
+function createProviderListItem(data) {
+	var $item = $('<li class="provider-list-item"></li>')
+	var $col1 = $('<div class="immugo-col immugo-col-70"></div>')
+	var $col2 = $('<div class="immugo-col immugo-col-30" style="text-align: right;"></div>')
+	var $button = $('<button class="immugo-button">Details</button>')
+	$button.click(function() {
+		window.open(data.link, )
+	})
+	$col1.append('<strong>' + data.name  + '</strong>')
+	$col1.append('<address>' + data.address + '</address>')
+	$col2.append($button)
+	$item.append($col1)
+	$item.append($col2)
+	return $item
 }
